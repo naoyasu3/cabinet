773ea15750515ed90b2495bb36cbfbdb20876897
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-30 14:03:08 +0900

    devise model Userに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_045202) do
+ActiveRecord::Schema.define(version: 2020_03_30_050237) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_045202) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "models", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 2020_03_30_045202) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
