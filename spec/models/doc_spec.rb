fdf5d9c51f66782837c69867762b23d23cd297ed
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-31 15:33:34 +0900

    doc model test Red

--- spec/models/doc_spec.rb
+++ spec/models/doc_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Doc, type: :model do
-  
+  let(:user){ FactoryBot.create(:user) }
+  let(:doc) { FactoryBot.build(:doc, user: user) }
+
+  it 'title, contextを入力した場合登録できる' do
+    doc = Doc.new(
+      title: "test title",
+      content: 'test text',
+      user: user,
+    )
+    expect(doc).to be_valid
+  end
+  it 'contentが空白でも成功すること' do
+    doc.content = nil
+    expect(doc).to be_valid  
+  end
+  it { is_expected.to  validate_presence_of :title }
 end
