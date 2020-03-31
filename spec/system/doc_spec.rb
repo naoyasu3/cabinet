edf62eb72fa3a6de2f3dbbac048eb2cc8c9f3988
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-03-31 18:49:21 +0900

    テストケースとテスト準備

--- spec/system/doc_spec.rb
+++ spec/system/doc_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Doc", type: :system, js: true do
+  let(:user) { FactoryBot.create(:user) }
 
+  feature 'Docの統合テスト' do
+    before do
+      sign_in_as user
+    end
+    scenario '一覧が表示されていること'
+    scenario '新規作成できること'
+    scenario '編集できること'
+    scenario '削除できること'
+  end
 end
